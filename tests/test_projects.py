3e6f59bd54081d4fea2b3eb0157c4b08612269d6
Karsten Schulz <github@karstenschulz.biz>
2022-06-12 11:09:38 +0200

    More tests

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -3,7 +3,7 @@ Test Project and Issues classes
 """
 import pytest
 
-from ytissues.ytlib import Project
+from ytissues.ytlib import Project, print_as_list, print_as_table
 
 
 class TestProject:
@@ -29,6 +29,50 @@ class TestProject:
     def test_plain_text(self):
         p = Project("P_ID")
         assert p.as_plaintext() == "P_ID None None"
+        assert p.displayname == "P_ID"
+
+    def test_displayname(self):
+        p = Project("0-1")
+        assert p.displayname == "0-1"
+        assert str(p) == "0-1"
+        p = Project("0-1", "ShortName")
+        assert p.displayname == "ShortName"
+        assert str(p) == "ShortName"
+        p = Project("0-1", "ShortName")
+        assert p.displayname == "ShortName"
+        assert str(p) == "ShortName"
+        p = Project("0-1", "ShortName", "Name")
+        assert p.displayname == "ShortName"
+        assert str(p) == "ShortName"
+
+    def test_equality(self):
+        p1 = Project("0-1")
+        p2 = Project("0-1")
+        p3 = Project("1-99")
+        assert p1 == p2
+        assert p1 != p3
+
+    def test_project_print(self, capfd):
+        p1 = Project("0-1")
+        p1.print(plain=True)
+        out, err = capfd.readouterr()
+        assert out == "0-1 None None\n"
+
+    def test_as_print_list(self, list_5_projects, capfd):
+        print_as_list(list_5_projects)
+        out, err = capfd.readouterr()
+        for i in range(0, 5):
+            assert list_5_projects[i].displayname in out
+        assert err == ""
+
+    def test_print_as_table(self, list_5_projects, capfd):
+        print_as_table(list_5_projects)
+        out, err = capfd.readouterr()
+        for i in range(0, 5):
+            assert list_5_projects[i].project_id in out
+            assert list_5_projects[i].shortname in out
+            assert list_5_projects[i].name in out
+        assert err == ""
 
     @pytest.mark.parametrize("init, result", data_displaynames)
     def test_displayname_text(self, init, result):
